2d51c71d8ecd1cc0cc9cccf58a6e30f84448ef1c
werberthcarvalho <werberth.gob@hotmail.com>
2025-12-24 12:38:57 -0300

    Create dados_aulas.php

---
+++ dados_aulas.php
@@ -0,0 +1,17 @@
+<?php
+// dados_aulas.php
+
+$lessonLinks = [
+    1 => "https://drive.google.com/file/d/1CE-69pxxZUmYurqp6-C7KplNfW9ewtXP/view?usp=drive_link",
+    2 => "https://drive.google.com/file/d/1CJ8LfneXsEO9tXS4wocR8JcyDUAI_E7B/view?usp=drive_link",
+    // Adicione os demais conforme necessário...
+];
+
+$lessonLinksSecondary = [
+    1 => "https://drive.google.com/file/d/0ByuJiBLnCPXgX3R0SW15bGJUS1E/view?usp=drive_link",
+    2 => "https://drive.google.com/file/d/12jpkWf6m2ST069drWA86rpSrSO8f5Yuc/view?usp=drive_link",
+    // Adicione os demais...
+];
+
+// Títulos e descrições podem ser mantidos no JS ou movidos para cá se desejar total controle via PHP.
+?>
